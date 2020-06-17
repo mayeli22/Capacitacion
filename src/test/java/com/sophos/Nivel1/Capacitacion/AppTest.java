dfc5628f0477891e631e0b9acd06994c9c568b9d
Mayeli <mayelipatilopez@gmail.com>
2020-06-16 19:29:05 -0500

    Primer Commit

---
+++ src/test/java/com/sophos/Nivel1/Capacitacion/AppTest.java
@@ -0,0 +1,20 @@
+package com.sophos.Nivel1.Capacitacion;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
